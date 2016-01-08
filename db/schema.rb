ef585ada444087a43d3f9bb8ba492251be7c4edb
Evelyn <evieb16@gmail.com>
2016-01-08 00:20:20 -0800

    Removes band table

--- db/schema.rb
+++ db/schema.rb
@@ -16,14 +16,6 @@ ActiveRecord::Schema.define(version: 20160106193639) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "bands", force: :cascade do |t|
-    t.string   "name"
-    t.string   "genre"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string   "image_file"
-  end
-
   create_table "pictures", force: :cascade do |t|
     t.text     "name"
     t.text     "description"
