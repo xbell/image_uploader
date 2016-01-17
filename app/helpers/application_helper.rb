223d6f78aae0852a2d07da45912a385b80dd3dc2
Evelyn <evieb16@gmail.com>
2016-01-16 19:07:31 -0800

    Rating counts visible on individual photo page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,9 +5,4 @@ module ApplicationHelper
       image_tag "http://www.gravatar.com/avatar/#{hashed_email}.jpg"
     end
 
-    def small_gravatar_for(email)
-      hashed_email = Digest::MD5.hexdigest(email.downcase.strip)
-      image_tag "http://www.gravatar.com/avatar/#{hashed_email}.jpg"
-    end
-
 end
