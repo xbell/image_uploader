e003b029a985242036d6f03f8b1d42958ba2a6fd
Evelyn <evieb16@gmail.com>
2016-01-21 22:14:17 -0800

    Fixes outgoing emails for rated photos

--- app/mailers/rating_mailer.rb
+++ app/mailers/rating_mailer.rb
@@ -2,16 +2,18 @@ class RatingMailer < ApplicationMailer
 
   def rating_email(user_id, review_id)
     @user = User.find(user_id)
-    mail(to: @user.email, subject: "One of your photos has been rated!")
-
     @review = Review.find(review_id)
+    @picture = Picture.find(@review.picture_id)
+
     @rating = "okay"
     if @review.rating == 1
       @rating = "thumbs up"
     elsif @review == -1
       @rating = "thumbs down"
     end
-    @picture = Picture.find(@review.picture_id)
+
+    mail(to: @user.email, subject: "One of your photos has been rated!")
+
   end
 
 end
